b05d44fd3d2eb994a1367bd087dc455b51856a9a
skcit1995 <skcit1995@gmail.com>
2026-01-09 13:42:54 +0530

    bash script

---
+++ Bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Welcome to script"
+
+mkdir sonam
+echo "Directory 'sonam' created"
+
+cd sonam
+echo "Changed directory to sonam"
+echo "creating new file"
+echo "Hello, this is a sample file" > sanja.txt
+
+echo "reading sanja.txt file"
+cat sanjay.txt
+echo "File content displayed successfully"
